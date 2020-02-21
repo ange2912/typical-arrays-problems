0603103b2a88482d4fd4148ecb7f75e85d2f0aee
Henadz Chumakou <genachumakov@mail.ru>
2020-02-21 19:22:45 +0300

    feat: add solution typical_arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (numbers) {
+  let result = 0;
+  if (!Array.isArray(numbers) || numbers.length==0) {return result = 0
+  } else for (let i=0; i<numbers.length; i++){
+    if(numbers[i]<result){
+      result = numbers[i]
+    }
+  }
+  return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (numbers) {
+  let result = 0;
+  if (!Array.isArray(numbers) || numbers.length==0) {return result = 0
+  } else for (let i=0; i<numbers.length; i++){
+    if(numbers[i]>result){
+      result = numbers[i]
+    }
+  }
+  return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (numbers){
+  let result = 0;
+  if (!Array.isArray(numbers) || numbers.length==0) {return result = 0
+  } else for (let i=0; i<numbers.length; i++){
+    result+=numbers[i]
+  }
+  return result/numbers.length;
 }
